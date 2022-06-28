cf762c9b88a6ceb84d96d27f6db653704935b2dd
RutaTang <rutatangpersonal@gmail.com>
2022-06-28 17:48:04 +0800

    finsih (un)ordered list but has direct insert err

--- src/Editor/slate-plugins/withList.jsx
+++ src/Editor/slate-plugins/withList.jsx
@@ -1,11 +1,35 @@
 import { Editor, Range, Transforms, Text, Point, Path, Element } from "slate";
 import { Circle } from "lucide-react";
 
-//TODO: add ordered list feature
 const renderListElement = ({ attributes, children, element }) => {
   switch (element.type) {
     case "ELEMENT_LIST_ORDERED_WRAPPER": {
-      return <ol {...attributes}>{children}</ol>;
+      return (
+        <ol
+          {...attributes}
+          style={{
+            marginLeft: `${Number(element.level)}rem`,
+          }}
+        >
+          {children.map((child, idx) => {
+            const isIncludNewWrapperInChildren = element.children[
+              idx
+            ].children.some(
+              (item) =>
+                item.type === "ELEMENT_LIST_ORDERED_WRAPPER" ||
+                item.type === "ELEMENT_LIST_UNORDERED_WRAPPER"
+            );
+            return (
+              <li key={idx} className="flex items-start">
+                {/* {isIncludNewWrapperInChildren ? null : ( */}
+                <span className="mr-2">{`${idx + 1}.`}</span>
+                {/* )} */}
+                {child}
+              </li>
+            );
+          })}
+        </ol>
+      );
     }
     case "ELEMENT_LIST_UNORDERED_WRAPPER": {
       return (
@@ -15,20 +39,28 @@ const renderListElement = ({ attributes, children, element }) => {
             marginLeft: `${Number(element.level)}rem`,
           }}
         >
-          {children}
+          {children.map((child, idx) => {
+            const isIncludNewWrapperInChildren = element.children[
+              idx
+            ].children.some(
+              (item) =>
+                item.type === "ELEMENT_LIST_ORDERED_WRAPPER" ||
+                item.type === "ELEMENT_LIST_UNORDERED_WRAPPER"
+            );
+            return (
+              <li key={idx} className="flex items-start">
+                {/* {isIncludNewWrapperInChildren ? null : ( */}
+                <Circle className="mr-2" size="0.8rem" />
+                {/* )} */}
+                {child}
+              </li>
+            );
+          })}
         </ul>
       );
     }
     case "ELEMENT_LIST_ITEM": {
-      const { showListItemIcon } = element;
-      return (
-        <li {...attributes} className="flex items-center">
-          <div contentEditable={false}>
-            {showListItemIcon ? <Circle size="0.6rem" /> : ""}
-          </div>
-          <div className="ml-2">{children}</div>
-        </li>
-      );
+      return <div {...attributes}>{children}</div>;
     }
     default: {
       return null;
@@ -42,13 +74,21 @@ const createListUnorderedWrapperElement = ({ level }) => {
     level,
   };
 };
-const createListItemElement = ({ showListItemIcon }) => {
-  return { type: "ELEMENT_LIST_ITEM", showListItemIcon };
+const createListOrderedWrapperElement = ({ level }) => {
+  return {
+    type: "ELEMENT_LIST_ORDERED_WRAPPER",
+    level,
+  };
+};
+const createListItemElement = () => {
+  return { type: "ELEMENT_LIST_ITEM" };
 };
 
 const withList = (editor) => {
-  const { deleteBackward, insertText, insertSoftBreak, insertBreak } = editor;
+  const { insertText, insertBreak } = editor;
+
   editor.insertText = (text) => {
+    //TODO: insert err
     const { selection } = editor;
     if (text === " " && selection && Range.isCollapsed(selection)) {
       const { anchor } = selection;
@@ -56,7 +96,9 @@ const withList = (editor) => {
         match: (n) => Editor.isBlock(editor, n),
       });
       const parentLisNodeEntry = Editor.above(editor, {
-        match: (n) => n.type == "ELEMENT_LIST_UNORDERED_WRAPPER",
+        match: (n) =>
+          n.type == "ELEMENT_LIST_UNORDERED_WRAPPER" ||
+          n.type == "ELEMENT_LIST_ORDERED_WRAPPER",
       });
       const parentListItemNodeEntry = Editor.above(editor, {
         match: (n) => n.type == "ELEMENT_LIST_ITEM",
@@ -67,30 +109,27 @@ const withList = (editor) => {
         const start = Editor.start(editor, path);
         const range = { anchor, focus: start };
         const beforeText = Editor.string(editor, range);
-        const isMached = /^(\*)\1*$/.test(beforeText);
-        if (isMached && !Editor.isEditor(node)) {
-          const numberOfQuote = beforeText.length;
+        const isMachedUnOrdered = /^(\*)\1*$/.test(beforeText);
+        const isMachedOrdered = /^(\d\.)\1*$/.test(beforeText);
+        if ((isMachedOrdered || isMachedUnOrdered) && !Editor.isEditor(node)) {
+          const numberOfQuote = isMachedUnOrdered
+            ? beforeText.length
+            : beforeText.length / 2;
           Transforms.delete(editor, { at: range });
           for (let idx = 0; idx < numberOfQuote; idx++) {
             let level = idx;
             if (parentLisNodeEntry && parentListItemNodeEntry) {
               level = parentLisNodeEntry[0].level + idx + 1;
-              Transforms.setNodes(
-                editor,
-                { showListItemIcon: false },
-                {
-                  at: parentListItemNodeEntry[1],
-                }
-              );
             }
-            Transforms.wrapNodes(
-              editor,
-              createListUnorderedWrapperElement({ level: level })
-            );
-            Transforms.wrapNodes(
-              editor,
-              createListItemElement({ showListItemIcon: true })
-            );
+            let listWrapper = null;
+            if (isMachedUnOrdered) {
+              listWrapper = createListUnorderedWrapperElement({ level: level });
+            }
+            if (isMachedOrdered) {
+              listWrapper = createListOrderedWrapperElement({ level: level });
+            }
+            Transforms.wrapNodes(editor, listWrapper);
+            Transforms.wrapNodes(editor, createListItemElement());
           }
           return;
         }
@@ -98,47 +137,6 @@ const withList = (editor) => {
     }
     insertText(text);
   };
-  // editor.deleteBackward = (...args) => {
-  //   const { selection } = editor;
-  //   if (selection && Range.isCollapsed(selection)) {
-  //     const parentNodeEntry = Editor.above(editor, {
-  //       match: (n) => Editor.isBlock(editor, n) && n.type == "ELEMENT_QUOTE",
-  //     });
-  //     if (parentNodeEntry) {
-  //       const [, path] = parentNodeEntry;
-  //       const start = Editor.start(editor, path);
-  //       if (Point.equals(start, selection.anchor)) {
-  //         Transforms.unwrapNodes(editor, {
-  //           match: (n) => n.type == "ELEMENT_QUOTE",
-  //         });
-  //         Transforms.insertText(editor, ">", {});
-  //         return;
-  //       }
-  //     }
-  //   }
-  //   deleteBackward(...args);
-  // };
-  // editor.insertSoftBreak = (...args) => {
-  //   const { selection } = editor;
-  //   if (selection && Range.isCollapsed(selection)) {
-  //     const parentNodeEntry = Editor.above(editor, {
-  //       match: (n) => Editor.isBlock(editor, n) && n.type == "ELEMENT_QUOTE",
-  //     });
-  //     if (parentNodeEntry) {
-  //       const [, path] = parentNodeEntry;
-  //       const end = Editor.end(editor, path);
-  //       if (Point.equals(end, selection.anchor)) {
-  //         insertSoftBreak(...args);
-  //         Transforms.unwrapNodes(editor, {
-  //           match: (n) => n.type == "ELEMENT_QUOTE",
-  //           split: true,
-  //         });
-  //         return;
-  //       }
-  //     }
-  //   }
-  //   insertSoftBreak(...args);
-  // };
   editor.insertBreak = (...args) => {
     const { selection } = editor;
     if (selection && Range.isCollapsed(selection)) {
@@ -155,10 +153,7 @@ const withList = (editor) => {
             split: true,
             match: (n) => n.type === "ELEMENT_LIST_ITEM",
           });
-          Transforms.wrapNodes(
-            editor,
-            createListItemElement({ showListItemIcon: true })
-          );
+          Transforms.wrapNodes(editor, createListItemElement());
           return;
         }
       }
@@ -186,14 +181,18 @@ const onKeyDown = (editor, e) => {
         if (isSelectionInListItemRange) {
           const ancestentListUnorderedWrapper = [
             ...Editor.nodes(editor, {
-              match: (n) => n.type === "ELEMENT_LIST_UNORDERED_WRAPPER",
+              match: (n) =>
+                n.type === "ELEMENT_LIST_UNORDERED_WRAPPER" ||
+                n.type === "ELEMENT_LIST_ORDERED_WRAPPER",
               mode: "all",
             }),
           ];
           if (ancestentListUnorderedWrapper.length > 1) {
             Transforms.unwrapNodes(editor, {
               split: true,
-              match: (n) => n.type === "ELEMENT_LIST_UNORDERED_WRAPPER",
+              match: (n) =>
+                n.type === "ELEMENT_LIST_UNORDERED_WRAPPER" ||
+                n.type === "ELEMENT_LIST_ORDERED_WRAPPER",
             });
             Transforms.liftNodes(editor, {
               split: true,
@@ -202,7 +201,9 @@ const onKeyDown = (editor, e) => {
           } else {
             Transforms.unwrapNodes(editor, {
               split: true,
-              match: (n) => n.type === "ELEMENT_LIST_UNORDERED_WRAPPER",
+              match: (n) =>
+                n.type === "ELEMENT_LIST_UNORDERED_WRAPPER" ||
+                n.type === "ELEMENT_LIST_ORDERED_WRAPPER",
             });
             Transforms.unwrapNodes(editor, {
               split: true,
@@ -222,36 +223,43 @@ const onKeyDown = (editor, e) => {
         at: selection,
         match: (n) => n.type === "ELEMENT_LIST_ITEM",
       });
-      if (listItemNodeEntry) {
-        const listItemRange = Editor.range(editor, listItemNodeEntry[1]);
-        const isSelectionInListItemRange = Range.includes(
-          listItemRange,
-          selection
-        );
-        if (isSelectionInListItemRange) {
-          const listWrapperNodeEntry = Editor.above(editor, {
-            match: (n) => n.type === "ELEMENT_LIST_UNORDERED_WRAPPER",
-          });
-          Transforms.wrapNodes(
-            editor,
-            createListUnorderedWrapperElement({
-              level: listWrapperNodeEntry[0].level + 1,
-            }),
-            {
-              at: listItemNodeEntry[1],
-            }
-          );
-          Transforms.wrapNodes(
-            editor,
-            createListItemElement({ showListItemIcon: false }),
-            {
-              at: Editor.above(editor, {
-                match: (n) => n.type === "ELEMENT_LIST_UNORDERED_WRAPPER",
-              })[1],
-            }
-          );
-        }
+      const listWrapperNodeEntry = Editor.above(editor, {
+        match: (n) =>
+          n.type === "ELEMENT_LIST_UNORDERED_WRAPPER" ||
+          n.type === "ELEMENT_LIST_ORDERED_WRAPPER",
+      });
+      if (!(listItemNodeEntry && listWrapperNodeEntry)) {
+        return;
       }
+      const lisItemIdx = listWrapperNodeEntry[0].children.findIndex(
+        (item) => item === listItemNodeEntry[0]
+      );
+      if (lisItemIdx <= 0) {
+        return;
+      }
+      //move ul to previous li
+      const destination = [
+        ...Path.previous(listItemNodeEntry[1]),
+        listWrapperNodeEntry[0].children[lisItemIdx - 1].children.length,
+      ];
+      Transforms.moveNodes(editor, {
+        to: destination,
+        at: listItemNodeEntry[1],
+      });
+      let wrapper = null;
+      if (listWrapperNodeEntry[0].type === "ELEMENT_LIST_ORDERED_WRAPPER") {
+        wrapper = createListOrderedWrapperElement({
+          level: listWrapperNodeEntry.level + 1,
+        });
+      }
+      if (listWrapperNodeEntry[0].type === "ELEMENT_LIST_UNORDERED_WRAPPER") {
+        wrapper = createListUnorderedWrapperElement({
+          level: listWrapperNodeEntry.level + 1,
+        });
+      }
+      Transforms.wrapNodes(editor, wrapper, {
+        at: destination,
+      });
     }
   }
   return;
